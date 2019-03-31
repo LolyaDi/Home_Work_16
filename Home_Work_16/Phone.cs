d1b1c32e6826d62b673fe371b5e1d2a2e8d2704d
LolyaDi <you@example.com>
2019-03-31 14:36:38 +0600

    Шестнадцатая домашняя работа

---
+++ Home_Work_16/Phone.cs
@@ -0,0 +1,13 @@
+﻿namespace Home_Work_16
+{
+    public class Phone
+    {
+        public string Brand { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public int Cost { get; set; }
+    }
+}
---
+++ Home_Work_16/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Home_Work_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type firstType = Type.GetType("System.Console");
+
+            Console.WriteLine("Методы System.Console - ");
+            foreach (var method in firstType.GetMethods())
+            {
+                Console.WriteLine(method.Name);
+            }
+
+            Phone phone = new Phone()
+            {
+                Model = "A5",
+                Brand = "Samsung",
+                Color = "Pink",
+                Cost = 75000
+            };
+
+
+
+
+            Console.WriteLine("Свойства класса - Phone: ");
+
+            Type secondType = phone.GetType();
+
+
+
+            foreach (var properties in secondType.GetProperties())
+            {
+                Console.WriteLine($"{properties.Name} - {properties.GetValue(phone, null)}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
